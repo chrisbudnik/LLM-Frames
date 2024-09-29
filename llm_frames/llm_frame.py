56995fd4ea1c11fbd5f6cd6b510457c088845a32
chrisb <chrism.budnik@gmail.com>
2024-09-29 23:40:28 +0200

    add input token estimation

--- llm_frames/llm_frame.py
+++ llm_frames/llm_frame.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
 import pandas as pd
+from typing import List, Dict
 from .operations import SemanticOperation
-from typing import List
+import tiktoken
 
 
 class LLMFrame:
@@ -19,16 +21,15 @@ class LLMFrame:
 
     def _apply_operation(self, operation: SemanticOperation, row: pd.Series) -> str:
         """Applies the operation to a single row of the DataFrame."""
-        
+
         context = row[operation.input_column]
         messages = operation.construct_messages(context)
         
-        response = self.client.chat.completions.create(
+        response: ChatCompletion = self.client.chat.completions.create(
             model=operation.model_name or "gpt-4o-mini",
             messages=messages,
             **operation.model_params or {}
         )
-        
         return response.choices[0].message.content
     
     def create_batch_file(
@@ -50,5 +51,28 @@ class LLMFrame:
                 f.write(str(batch) + "\n")
 
         print(f"Batch file created: {batch_file_name}")
+
+    def estimate_input_token_usage(self, operation: SemanticOperation) -> Dict:
+        """Estimates the token usage for an operation on the DataFrame."""
+
+        token_counts: pd.Series = self.df.apply(
+            lambda row: self._count_token_usage(row[operation.input_column], operation.model_name),
+            axis=1
+        )
+        token_usage = {
+            "total_tokens": token_counts.sum(),
+            "average_tokens": token_counts.mean(),
+            "max_tokens": token_counts.max(),
+            "min_tokens": token_counts.min()
+        }
+        return token_usage
+
+    def _count_token_usage(self, text: str, model_name: str) -> int:
+        """Returns the number of tokens in a text string."""
+        
+        encoding = tiktoken.encoding_for_model(model_name)
+        num_tokens = len(encoding.encode(text))
+        return num_tokens
+
 
 
